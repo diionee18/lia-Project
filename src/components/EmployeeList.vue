b2228b947ed4169f4107a07cae800a928c2678ad
Ali Al-Nasir <iraki-ali17@hotmail.com>
2023-07-08 16:48:56 +0200

    Design
    Design ser ut och vara klar så lägger det för testning och återkommer till det senare ifall jag ska tillägga något mer.

--- src/components/EmployeeList.vue
+++ src/components/EmployeeList.vue
@@ -1,14 +1,17 @@
 <template>
-  <div>
-    <ul class="user-wrapper">
-      <li class="users-li" v-for="employee in employees" :key="employee.id">
-        <img class="user-img" :src="employee.avatar" alt="profilbild">
-       <p class="fullname"> {{  employee.first_name }} {{ employee.last_name }} </p>
-        <a :href="'mailto:' + employee.email">Kontakt</a>
-      </li>
-    </ul>
-</div>
-</template>
+    <div>
+      <canvas class="canvas"></canvas>
+      <ul class="user-wrapper">
+        <li class="users-li" v-for="(employee, index) in employees" :key="employee.id" >
+          <img class="user-img" :src="employee.avatar" alt="profilbild">
+          <canvas class="background-canvas" :style="{ backgroundColor: index % 2 === 0 ? color1 : color2 }"></canvas>
+          <p class="fullname">{{ employee.first_name }} {{ employee.last_name }}</p>
+          <a class="contact" :href="'mailto:' + employee.email" :style="{ backgroundColor: index % 2 === 0 ? color1 : color2 }"  >Kontakt</a>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
 
   <script src="../utils/EmployeeListScript"></script>
 
--- src/utils/EmployeeListScript.js
+++ src/utils/EmployeeListScript.js
@@ -1,27 +1,35 @@
+import {getRandomColor} from './RandomColor'
 
 export default {
-    data(){
-        return{
-            employees: [],
-            currentPage: this.intialPage,
-            totalPages: null
-        };
-    },
+    data() {
+      return {
+        employees: [],
+        currentPage: this.intialPage,
+        totalPages: null,
+        color1: '#d476b8', 
+        color2: '#20478e'
 
+      };
+    },
+  
     mounted() {
-        this.fetchEmployees();
+      this.fetchEmployees();
     },
     methods: {
-        fetchEmployees() {
-            fetch(`https://reqres.in/api/users?page`)
-            .then(response => response.json())
-            .then(data => {
-                this.employees = data.data;
-                this.totalPages = data.total_pages;
-            })
-            .catch(error => {
-                console.error('Ett fel har inträffat: ' + error)
-            })
-        }
+      fetchEmployees() {
+        fetch(`https://reqres.in/api/users?page`)
+          .then(response => response.json())
+          .then(data => {
+            this.employees = data.data.map(employee => ({
+              ...employee,
+            //   backgroundColor: getRandomColor()
+            }));
+            this.totalPages = data.total_pages;
+          })
+          .catch(error => {
+            console.error('Ett fel har inträffat: ' + error)
+          })
+      }
     },
-};
+  };
+  
